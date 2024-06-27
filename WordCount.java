092b18945c642e5e849e4afe2a11206b3bb15520
jarman333 <113035716+jarman333@users.noreply.github.com>
2024-06-26 23:57:34 -0400

    Update WordCount.java
    cambia gs del proyecto simulando un libro de una palabra para ejecutar la llamada al restful solo una vez

--- WordCount.java
+++ WordCount.java
@@ -228,7 +228,7 @@ public class WordCount {
      * this option to choose a different input file or glob.
      */
     @Description("Path of the file to read from")
-    @Default.String("gs://dataflow-apache-quickstart_dataflowbatchjsoncsv/entrada/leo.txt")
+    @Default.String("gs://dataflow-apache-quickstart_booming-bonito-420721/entrada/julio.txt")
     String getInputFile();
 
     void setInputFile(String value);
